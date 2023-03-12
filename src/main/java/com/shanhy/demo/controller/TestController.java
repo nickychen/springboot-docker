dfd0c7b10a749d57b3806d51b0f027ddfd3e9839
nickychen <snc21@163.com>
2023-03-12 22:15:55 +0800

    增加变量测试

--- src/main/java/com/shanhy/demo/controller/TestController.java
+++ src/main/java/com/shanhy/demo/controller/TestController.java
@@ -1,13 +1,15 @@
 package com.shanhy.demo.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
-
+    @Value("${server.name}")
+    private String name;
 	@GetMapping
 	public String show() {
-		return "OK888";
+		return "OK888" + name;
 	}
 }
